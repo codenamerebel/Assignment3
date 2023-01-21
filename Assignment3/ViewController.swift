b1d65d77ed2e8fbe9b3ce0055d88daf6a1987462
Brad Payne <brad@thepaynefamily.org>
2023-01-20 23:39:11 -0600

    Set time to appear on label.  Just needs to update now.  Most of this is figuring out swift vs knowing Obj C

--- Assignment3/ViewController.swift
+++ Assignment3/ViewController.swift
@@ -8,10 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet   var currentTimeLabel: UILabel!;
+    
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+
+        //  Get the current date and format it
+        //  Format is Wed, 28 Dec 2022 14:59:00
+        var currentTime:Date = Date();
+        let dateFormatter:DateFormatter = DateFormatter();
+        dateFormatter.dateFormat = "E, d MMM Y HH:mm:ss"
+        self.currentTimeLabel.text = dateFormatter.string(from: currentTime);
+        
+        
     }
 
 
