73587fb598660b1e26d495e2c48b9d7e98ecccc1
Brad Payne <brad@thepaynefamily.org>
2023-01-21 22:53:10 -0600

    Updated to be able to detect AM or PM with the main clock

--- Assignment3/ViewController.swift
+++ Assignment3/ViewController.swift
@@ -7,7 +7,8 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
     
     @IBOutlet   var currentTimeLabel: UILabel!;
     
@@ -17,16 +18,45 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
 
-        //  Get the current date and format it
-        //  Format is Wed, 28 Dec 2022 14:59:00
-        var currentTime:Date = Date();
-        let dateFormatter:DateFormatter = DateFormatter();
-        dateFormatter.dateFormat = "E, d MMM Y HH:mm:ss"
-        self.currentTimeLabel.text = dateFormatter.string(from: currentTime);
-        
+        //  Initiate the updates for the clock.  It should trigger every second
+        //  It actually has a little wiggle room and isn't exact but it's close enough
+        //  for the GUI
+        Timer.scheduledTimer(timeInterval:1.0, target:self, selector: #selector(self.UpdateCurrentTime), userInfo:nil, repeats: true);
         
     }
 
+    @objc   func UpdateCurrentTime()
+    {
+        //  Get the current date and format it
+        //  Format is Wed, 28 Dec 2022 14:59:00
+            
+        let currentDate = Date();
+        let dateFormatter:DateFormatter = DateFormatter();
+        dateFormatter.dateFormat = "E, d MMM Y HH:mm:ss";
+        self.currentTimeLabel.text = dateFormatter.string(from: currentDate);
+
+        
+        //  Honestly I know there are ways to do this with the actual Date
+        //  but I hate dealing with dates or NSdate (Obj C) so I went the cheap way!
+        let dateHourFormatter:DateFormatter = DateFormatter();
+        dateHourFormatter.dateFormat = "HH";
+
+        let hour:String = dateHourFormatter.string(from: currentDate)
+      
+        //  Need to update to change background picture
+        if(Int(hour)! < 12)
+        {
+            print("AM")
+        }
+        else
+        {
+            print("PM")
+        }
+
+    }
+    
+    
+    
 
 }
 
